c606191f7b616e4a64cba292f6f1aab9c3b2bbf9
Sajal Srivastava <sajalsrivastava123123aa@gmail.com>
2023-10-10 09:42:15 +0530

    2nd commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
- 
-function Card(){
-
+ import './Card.css';
+function Card(props){
+    const classes='card '+props.className;
+    return <div className={classes}>{props.children}</div>
 }
 
 export default Card;
